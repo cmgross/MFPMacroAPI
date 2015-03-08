a5dd3601ca6e10cb462507c0c22a165c72c9cb58
Charles Gross <chuckgross@gmail.com>
2015-03-08 15:57:29 -0400

    Added csv media formatter to get with data param

--- MFPMacroAPI/Controllers/NutritionValuesController.cs
+++ MFPMacroAPI/Controllers/NutritionValuesController.cs
@@ -23,16 +23,6 @@ namespace MFPMacroAPI.Controllers
             //todo check parameters
             //call screenscrape library with parameters, check the return type (error, etc)
             var results = Scraper.Scraper.Scrape(userName, date);
-            return Request.CreateResponse(HttpStatusCode.OK, GetValues(results));
-        }
-
-        //GET api/nutritionvalues/chuckgross/
-        public HttpResponseMessage GetToday(string userName)
-        {
-            //todo check parameters
-            //call screenscrape library with parameters, check the return type (error, etc)
-            var results = Scraper.Scraper.Scrape(userName);
-            //return Request.CreateResponse(HttpStatusCode.OK, GetValues(results));
             var responseMessage = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(GetValues(results)) };
             responseMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
             responseMessage.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
@@ -42,27 +32,20 @@ namespace MFPMacroAPI.Controllers
             return responseMessage;
         }
 
-        //[HttpPost]
-        //public HttpResponseMessage GenerateCSV(FieldParameters fieldParams)
-        //{
-        //    var output = new byte[] { };
-        //    if (fieldParams != null)
-        //    {
-        //        using (var stream = new MemoryStream())
-        //        {
-        //            this.SerializeSetting(fieldParams, stream);
-        //            stream.Flush();
-        //            output = stream.ToArray();
-        //        }
-        //    }
-        //    var result = new HttpResponseMessage(HttpStatusCode.OK) { Content = new ByteArrayContent(output) };
-        //    result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-        //    result.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
-        //    {
-        //        FileName = "File.csv"
-        //    };
-        //    return result;
-        //}
+        //GET api/nutritionvalues/chuckgross/
+        public HttpResponseMessage GetToday(string userName)
+        {
+            //todo check parameters
+            //call screenscrape library with parameters, check the return type (error, etc)
+            var results = Scraper.Scraper.Scrape(userName);
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(GetValues(results)) };
+            responseMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            responseMessage.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = "results.csv"
+            };
+            return responseMessage;
+        }
 
         private string GetValues(Scraper.Scraper.NutritionRecord nutritrionRecord)
         {
