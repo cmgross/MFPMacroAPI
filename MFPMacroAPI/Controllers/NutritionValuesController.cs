73113ef2bbddcdaac35a02f3e595684e356ffeaf
Charles Gross <chuckgross@gmail.com>
2015-03-08 15:54:32 -0400

    Fixed csv implementation

--- MFPMacroAPI/Controllers/NutritionValuesController.cs
+++ MFPMacroAPI/Controllers/NutritionValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Web.Http;
 
@@ -31,8 +32,37 @@ namespace MFPMacroAPI.Controllers
             //todo check parameters
             //call screenscrape library with parameters, check the return type (error, etc)
             var results = Scraper.Scraper.Scrape(userName);
-            return Request.CreateResponse(HttpStatusCode.OK, GetValues(results));
+            //return Request.CreateResponse(HttpStatusCode.OK, GetValues(results));
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(GetValues(results)) };
+            responseMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            responseMessage.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = "results.csv"
+            };
+            return responseMessage;
         }
+
+        //[HttpPost]
+        //public HttpResponseMessage GenerateCSV(FieldParameters fieldParams)
+        //{
+        //    var output = new byte[] { };
+        //    if (fieldParams != null)
+        //    {
+        //        using (var stream = new MemoryStream())
+        //        {
+        //            this.SerializeSetting(fieldParams, stream);
+        //            stream.Flush();
+        //            output = stream.ToArray();
+        //        }
+        //    }
+        //    var result = new HttpResponseMessage(HttpStatusCode.OK) { Content = new ByteArrayContent(output) };
+        //    result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+        //    result.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+        //    {
+        //        FileName = "File.csv"
+        //    };
+        //    return result;
+        //}
 
         private string GetValues(Scraper.Scraper.NutritionRecord nutritrionRecord)
         {
