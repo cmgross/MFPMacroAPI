a88d72f3a335e2424297aaefceef84b6c770abda
Charles Gross <chuckgross@gmail.com>
2015-03-08 15:33:27 -0400

    Added values only controller
    For use in Google Sheets via ImportJSON

---
+++ MFPMacroAPI/Controllers/NutritionValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MFPMacroAPI.Controllers
+{
+    public class NutritionValuesController : ApiController
+    {
+        //GET api/nutritionvalues
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.BadRequest);
+        }
+
+        //GET api/nutritionvalues/chuckgross/2015-03-05
+        public HttpResponseMessage Get(string userName, string date)
+        {
+            //todo check parameters
+            //call screenscrape library with parameters, check the return type (error, etc)
+            var results = Scraper.Scraper.Scrape(userName, date);
+            return Request.CreateResponse(HttpStatusCode.OK, GetValues(results));
+        }
+
+        //GET api/nutritionvalues/chuckgross/
+        public HttpResponseMessage GetToday(string userName)
+        {
+            //todo check parameters
+            //call screenscrape library with parameters, check the return type (error, etc)
+            var results = Scraper.Scraper.Scrape(userName);
+            return Request.CreateResponse(HttpStatusCode.OK, GetValues(results));
+        }
+
+        private List<string> GetValues(Scraper.Scraper.NutritionRecord nutritrionRecord)
+        {
+            var values = new List<string>
+            {
+                nutritrionRecord.Calories,
+                nutritrionRecord.Protein,
+                nutritrionRecord.Fat,
+                nutritrionRecord.Carbs
+            };
+            return values;
+        }
+    }
+}
--- MFPMacroAPI/Global.asax.cs
+++ MFPMacroAPI/Global.asax.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Formatting;
-using System.Net.Http.Headers;
-using System.Web;
+﻿using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 
 namespace MFPMacroAPI
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
