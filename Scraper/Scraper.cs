cf503b3029e0085d2d7acff2e5dcb7c470650c84
Charles Gross <chuckgross@gmail.com>
2015-09-06 15:20:04 -0400

    Added Fiber to return value

--- Scraper/Scraper.cs
+++ Scraper/Scraper.cs
@@ -34,21 +34,22 @@ namespace Scraper
             //    <td>273g</td> Carbs [1]
             //    <td>89g</td> Fat [2]
             //    <td>225g</td> Protein [3]
-            //    <td>838mg</td> Cholest
-            //    <td>1,848mg</td> Sodium 
-            //    <td>50g</td> Sugars
-            //    <td class="last">43g</td> Fiber
+            //    <td>838mg</td> Cholest [4]
+            //    <td>1,848mg</td> Sodium [5]
+            //    <td>50g</td> Sugars [6]
+            //    <td class="last">43g</td> Fiber [7]
             //</tr>
             //</tfoot>
 
             HtmlNode totalsRow = footer.Descendants("tr").First();
-            HtmlNodeCollection cells = totalsRow.SelectNodes(".//td[not(@class='first') and not(@class='last')]");
+            HtmlNodeCollection cells = totalsRow.SelectNodes(".//td[not(@class='first')]");
 
             var macros = new Macros
             {
                 Calories = cells[0].InnerText.Replace(",", string.Empty),
                 Carbs = cells[1].InnerText.Replace("g", string.Empty),
                 Fat = cells[2].InnerText.Replace("g", string.Empty),
+                Fiber = cells[7].InnerText.Replace("g", string.Empty),
                 Protein = cells[3].InnerText.Replace("g", string.Empty)
             };
             return macros;
@@ -76,6 +77,7 @@ namespace Scraper
             public string Protein { get; set; }
             public string Fat { get; set; }
             public string Carbs { get; set; }
+            public string Fiber { get; set; }
 
             public Macros()
             {
@@ -83,11 +85,12 @@ namespace Scraper
                 Protein = "0";
                 Fat = "0";
                 Carbs = "0";
+                Fiber = "0";
             }
 
             public override string ToString()
             {
-                return Calories + "," + Protein + "," + Fat + "," + Carbs;
+                return Calories + "," + Protein + "," + Fat + "," + Carbs + "," + Fiber;
             }
         }
 
